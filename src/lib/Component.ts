35f466f753c7b9f36469e4f561dc415295e19558
Fermain <Fermain@users.noreply.github.com>
2023-06-02 12:54:23 +0200

    Removes events for now

--- src/lib/Component.ts
+++ src/lib/Component.ts
@@ -1,13 +1,11 @@
 export class Component extends HTMLElement {
     shadowRoot: ShadowRoot;
-    protected eventTarget: EventTarget;
     static tagName = "noroff-component";
     static styles = "";
 
     constructor() {
         super();
         this.shadowRoot = this.attachShadow({ mode: 'open' });
-        this.eventTarget = new EventTarget();
     }
 
     static register() {
@@ -41,17 +39,5 @@ export class Component extends HTMLElement {
     protected onDestroy() { }
     protected onUpdate(name: string, oldValue: string, newValue: string) {
         console.log(`Attribute ${name} changed from ${oldValue} to ${newValue}`);
-    }
-
-    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) {
-        this.eventTarget.addEventListener(type, listener, options);
-    }
-
-    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions) {
-        this.eventTarget.removeEventListener(type, listener, options);
-    }
-
-    dispatchEvent(event: Event) {
-        return this.eventTarget.dispatchEvent(event);
     }
 }
